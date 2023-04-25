4df16161b6abd9c63e91c77577a9d619e022422f
Dave Prutch <prutch.david@gmail.com>
2023-04-25 22:36:19 +0000

    added notes

--- helloworld.sh
+++ helloworld.sh
@@ -3,7 +3,7 @@
 # Script Name:                  ops 201d8 Ops Challenge 02
 # Author:                       David Prutch
 # Date of latest revision:      04/25/2023
-# Purpose:                      learn to use echo to print to command line
+# Purpose:                      learn to use echo to print to command line and setting variables
 
 # Declaration of variables
 greeting="Hello World"
@@ -12,6 +12,8 @@ extra="I am working perfectly!"
 
 
 # Main
+# echo is command to print
+# $ signifies the following is a variable to differentiate it from a string
 echo $greeting
 echo $extra
 
