9015d3684dc5c7fc8385c6b99d495d0bc6f1f447
Dave Prutch <prutch.david@gmail.com>
2023-04-25 22:27:24 +0000

    first code commit

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  ops 201d8 Ops Challenge 02
+# Author:                       David Prutch
+# Date of latest revision:      04/25/2023
+# Purpose:                      learn to use echo to print to command line
+
+# Declaration of variables
+greeting="Hello World"
+extra="I am working perfectly!"
+# Declaration of functions
+
+
+# Main
+echo $greeting
+echo $extra
+
+
+# End
