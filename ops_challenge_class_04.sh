c8f0614f7085d211862fedb23730637b9bff1f3a
Dave Prutch <prutch.david@gmail.com>
2023-04-28 00:59:30 +0000

    challenge day 4 commit

---
+++ ops_challenge_class_04.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script Name:                  ops 201d8 Ops Challenge 04
+# Author:                       David Prutch
+# Date of latest revision:      04/27/2023
+# Purpose:                      Learning to use Arrays in bash
+
+# Declaration of variables
+#Create four directories: dir1, dir2, dir3, dir4
+dir1="dave_dir"
+dir2="mike_dir"
+dir3="rob_dir"
+dir4="chris_dir"
+file1="dave.txt"
+file2="mike.txt"
+file3="rob.txt"
+file4="chris.txt"
+#array
+directory_array=($dir1 $dir2 $dir3 $dir4)
+
+# Declaration of functions
+
+
+
+#append to array: array_name+=(article)
+
+# Main
+#Put the names of the four directories in an array
+mkdir $dir1
+mkdir $dir2
+mkdir $dir3
+mkdir $dir4
+
+#Reference the array variable to create a new .txt file in each directory
+touch  "/home/dave/ops-201d8-code-challenges/"${directory_array[0]}/$file1""
+touch "/home/dave/ops-201d8-code-challenges/"${directory_array[1]}/$file2""
+touch "/home/dave/ops-201d8-code-challenges/"${directory_array[2]}/$file3""
+touch "/home/dave/ops-201d8-code-challenges/"${directory_array[3]}/$file4""
+
+# END
