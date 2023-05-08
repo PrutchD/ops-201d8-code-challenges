1891ed81989dcc5979db20077430938a9b969409
Dave Prutch <prutch.david@gmail.com>
2023-05-08 23:38:27 +0000

    challenge day 11

---
+++ ops_challenge_class_11.ps1
@@ -0,0 +1,26 @@
+# Script Name:                  ops 201d8 Ops Challenge 11
+# Author:                       David Prutch
+# Date of latest revision:      05/05/2023
+# Purpose:                      Write a Powershell script that automates the configuration of a new Windows 10 endpoint.
+
+# Main
+
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+#Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+
+# Enable Remote management
+Enable-PSRemoting
+
+# Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Enable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+
+# End
